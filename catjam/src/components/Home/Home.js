b216972ec7b2cc7fae90f330e3e31911aaa6d927
brendang8 <55946089+brendang8@users.noreply.github.com>
2021-11-23 03:11:03 +0800

    Added image slider

--- catjam/src/components/Home/Home.js
+++ catjam/src/components/Home/Home.js
@@ -1,10 +1,56 @@
 import "./Home.css";
 import NavBar from "../NavBar/NavBar";
+import Carousel from "react-material-ui-carousel"
+import { Paper, Button } from '@mui/material'
 
 function Home() {
+
+    var items = [
+        {
+            name: "A whole world of freelance talent at your fingertips",
+            description: "Discover high quality services from a wide range of categories",
+            button: "Explore Now",
+            src: "https://theprofessionalcentre.com/media/freelancer-office.jpg"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!",
+            button: "Try Now",
+            src: "https://cms-assets.themuse.com/media/lead/_1200x630_crop_center-center_82_none/18447.jpg?mtime=1568865212"
+        }
+    ]
+
+
     return(
         <div>
             <NavBar/>
+            <Carousel className="carousel">
+                {
+                    items.map( (item, i) => <Item key={i} item={item} /> )
+                }
+            </Carousel>
+        </div>
+    )
+}
+
+function Item(props)
+{
+    return (
+        <div className="item">
+            <div className="item-image">
+                <img src={props.item.src} alt=''/>
+            </div>
+            <div className="item-content">
+                <div className="item-content-container">
+                    <span className="item-text-title">{props.item.name}</span>
+                    <span className="item-text">{props.item.description}</span>
+                    <Button variant="contained" color="secondary" style={{marginTop: '50px'}}>
+                        {props.item.button}
+                    </Button>
+                </div>
+                
+            </div>
+            
         </div>
     )
 }
--- catjam/src/components/NavBar/NavBar.js
+++ catjam/src/components/NavBar/NavBar.js
@@ -1,22 +1,25 @@
 import "./NavBar.css"
 import logo from '../../assets/catjam.png'
 
+import { Button, AppBar, Toolbar} from '@mui/material'
+
 function NavBar() {
     return (
-        <div className="navbar-main">
-            <div className="logo">
-                <img className="logo-picture" src={logo}/>
-                <span className="logo-text">catJAM</span>
-            </div>
-            <div className="navbar-option">
-                <span>Login</span>
-            </div>
-            <div className="navbar-option">
-                <span>Login</span>
-            </div>
-            <div className="navbar-option">
-                <span>Login</span>
-            </div>
+        <div>
+            <AppBar>
+                <Toolbar>
+                <div className="logo">
+                    <img className="logo-picture" src={logo}/>
+                    <span className="logo-text">catJAM</span>
+                </div>
+                <div className="navbar-option-container">
+                    <Button style={{ color: "white"}}> Sign In</Button>
+                    <Button style={{ color: "white"}}> Join</Button>
+                </div>
+                
+                </Toolbar>
+            </AppBar>
+            
         </div>
     )
 }
